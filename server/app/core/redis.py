db3eb5123efd68162a8bcb7e65ac9ee667abd24c
Muhammad Nizar <mnizarzr@gmail.com>
2022-05-12 00:51:05 +0700

    🏗️ detach redis client from app

--- server/app/core/redis.py
+++ server/app/core/redis.py
@@ -2,7 +2,21 @@ import aioredis
 
 from app.core.config import config
 
+redis: aioredis.Redis = None  # type: ignore
+
 
 async def init_redis_pool() -> aioredis.Redis:
+    global redis
     redis = await aioredis.from_url(config.REDIS_URL, decode_responses=True)
     return redis
+
+
+async def get_connection():
+    if redis is not None:
+        return redis
+    return await init_redis_pool()
+
+
+async def disconnect():
+    if redis is not None:
+        await redis.close()
--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,16 @@
 from beanie import init_beanie
 from fastapi import FastAPI
+
 from app.api.router import api_router
-from app.core.db import db, connect as connect_mongo, disconnect as disconnect_mongo
-from app.core.redis import init_redis_pool
+from app.core.db import (
+    db,
+    connect as connect_mongo,
+    disconnect as disconnect_mongo
+)
+from app.core.redis import (
+    init_redis_pool,
+    disconnect as disconnect_redis
+)
 from app.models import __beanie_models__
 
 app = FastAPI()
@@ -11,14 +19,17 @@ app = FastAPI()
 @app.on_event("startup")
 async def startup_event():
     await connect_mongo()
-    await init_beanie(database=db.database, document_models=__beanie_models__)
-    app.state.redis = await init_redis_pool()
+    await init_beanie(
+        database=db.database,
+        document_models=__beanie_models__
+    )
+    await init_redis_pool()
 
 
 @app.on_event("shutdown")
 async def shutdown_event():
     await disconnect_mongo()
-    await app.state.redis.close()
+    await disconnect_redis()
 
 
 app.include_router(api_router)
