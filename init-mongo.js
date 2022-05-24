6c91e408c290e2da4995da6d812594c0caeb0767
Muhammad Nizar <mnizarzr@gmail.com>
2022-05-25 01:39:59 +0700

    🌱 seed default admin

--- init-mongo.js
+++ init-mongo.js
@@ -9,4 +9,122 @@ db.createUser({
     ]
 });
 
-db.createCollection("test");
+db.createCollection("users");
+
+db.users.update(
+    {
+        email: "admin@admin.com"
+    },
+    {
+        $setOnInsert: { email: "admin@admin.com", name: "Administrator", password: "$2b$12$lOrFtpzzlWKtxV06Nhw1GOdCCMwVY.OU9iI3SU.L7PQpnbRVD9CNK" }
+    },
+    {
+        upsert: true
+    }
+)
+
+db.categories.insertMany(
+    [
+        {
+            "name": "Art",
+            "sub_categories": [
+                { "name": "Drawings" },
+                { "name": "Photography" },
+                { "name": "Mixed Media & Collages" },
+                { "name": "Sculptures" },
+                { "name": "Prints & Multiples" },
+                { "name": "Scrolls, Screens & Fan paintings" },
+                { "name": "Paintings" },
+                { "name": "Statues & Statuettes" }
+            ]
+        },
+        {
+            "name": "Furniture",
+            "sub_categories": [
+                { "name": "Bedframes & Headboards" },
+                { "name": "Tables" },
+                { "name": "Armoires & Wardrobes" },
+                { "name": "Sideboards & Buffets" },
+                { "name": "Chairs" },
+                { "name": "Bookcases, Etageres & Display Cases" },
+                { "name": "Sofas & Canapes" },
+                { "name": "Seating" },
+                { "name": "Benches & Stools" },
+                { "name": "Desks & Writing Tables" },
+                { "name": "Dining Tables" }
+            ]
+        },
+        {
+            "name": "Jewelry",
+            "sub_categories": [
+                { "name": "Cufflinks, Tie Pins & Tie Clips" },
+                { "name": "Necklaces & Pendants" },
+                { "name": "Costume Jewelry" },
+                { "name": "Brooches & Pins" },
+                { "name": "Loose Stones & Beads" },
+                { "name": "Earrings" },
+                { "name": "Rings" },
+                { "name": "Bracelets" },
+                { "name": "Watches" }
+            ]
+        },
+
+        {
+            "name": "Collectibles",
+            "sub_categories": [
+                { "name": "Figurines & Miniatures" },
+                { "name": "Home Hardware, Tools & Locks" },
+                { "name": "Hunting & Fishing" },
+                { "name": "Pens & Writing Instruments" },
+                { "name": "Maps,  Globes & Atlases" },
+                { "name": "Musical Instruments & Music Stands" },
+                { "name": "Coins, Currency & Stamps" },
+                { "name": "Cameras" },
+                { "name": "Books, Magazines & Papers" },
+                { "name": "Banks, Registers & Adding Machines" },
+                { "name": "Radios & Audio Equipment" },
+                { "name": "Weapons & Armor" },
+                { "name": "Scientific Objects" },
+                { "name": "Games" },
+                { "name": "Advertisements & Signs" }
+            ]
+        },
+        {
+            "name": "Fashion",
+            "sub_categories": [
+                { "name": "Dresses" },
+                { "name": "Jackets & Coats" },
+                { "name": "Handbags & Purses" },
+                { "name": "Belts" },
+                { "name": "Wallets" },
+                { "name": "Shoes" },
+                { "name": "Eyeglasses" },
+                { "name": "Sweaters" },
+                { "name": "Skirts" },
+                { "name": "Suits" },
+                { "name": "Shirts & Blouses" },
+                { "name": "Pants & Trousers" }
+            ]
+        },
+        {
+            "name": "Home & Garden",
+            "sub_categories": [
+                { "name": "Frames, Shelves & Wall Decors" },
+                { "name": "Mirrors" },
+                { "name": "Lighting" },
+                { "name": "Garden Ornaments" },
+                { "name": "Garden Furniture" },
+                { "name": "Clocks" },
+                { "name": "Fireplaces" },
+                { "name": "Fireplace Accessories" },
+                { "name": "Baskets & Containers" },
+                { "name": "Candle Holders" },
+                { "name": "Rugs & Carpets" },
+                { "name": "Textiles" },
+                { "name": "Umbrella Stands & Coat Stands" },
+                { "name": "Vases & Garnitures" },
+                { "name": "Plants" }
+            ]
+        }
+    ]
+)
