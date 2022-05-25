a06a7e5f566df26002a99eeea1e861db3c4a7c70
Zain5316 <luthfizain45@gmail.com>
2022-05-25 17:49:53 +0700

    ui login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import logo from "./logo.svg";
+import ilustrasi from "./assets/ilustrasi.svg";
 import "./App.css";
 
 const API_URL = "http://localhost:8080/v1";
@@ -13,30 +14,32 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <form class="form-control" onSubmit={handleLogin}>
-        <label class="label">
-          <span class="label-text">Your Email</span>
-        </label>
+    <div className="App h-screen flex justify-center items-center">
+      <div>
+      <form class="form-control flex items-center" onSubmit={handleLogin}>
         <label class="input-group">
-          <span>Email</span>
+          <span class="w-28">Email</span>
           <input
             type="text"
             placeholder="info@site.com"
             class="input input-bordered"
           />
         </label>
-        <label class="label">
-          <span class="label-text">Your Password</span>
+        <label class="input-group mt-4">
+          <span class="w-28">Password</span>
+          <input type="password" placeholder="Minimal 8 Karakter" class="input input-bordered" />
         </label>
-        <label class="input-group">
-          <span>Password</span>
-          <input type="password" class="input input-bordered" />
-        </label>
-        <button class="btn" type="submit">
+        {/* <button class="btn rounded-fullpx-20 text-white mt-10" type="submit">Login</button> */}
+        <button class="btn btn-primary px-20 text-white mt-10" type="submit">
           Login
         </button>
       </form>
+      </div>
+
+      <div class="flex justify-center">
+        <img src={ilustrasi} alt="ilustrasi" class="w-3/5" />
+      </div>
+      
     </div>
   );
 }
