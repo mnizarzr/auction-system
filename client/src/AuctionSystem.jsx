13b3e37489493baed998b58d29064f39e76ee013
Zain5316 <luthfizain45@gmail.com>
2022-06-15 14:55:36 +0700

    first commit

--- client/src/AuctionSystem.jsx
+++ client/src/AuctionSystem.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 export default function auctionsystem() {
     return (
         <body>
+
             <div class="navbar bg-neutral-focus">
                 <div class="tabs flex-1 mx-20">
                     <ul>
@@ -98,6 +99,38 @@ export default function auctionsystem() {
                     </div>
                 </div>
             </div>
+
+            <div class="flex">
+                <div class="navbar my-6 mx-24 w-full text-4xl font-semibold">
+                    <p>Hottest upcoming items</p>
+                </div>
+            </div>
+
+            {/* <div class="flex justify-center">
+                <div class="w-64 carousel rounded-box">
+                    <div class="carousel-item w-full">
+                        <img src="https://api.lorem.space/image/game?w=256&h=400&hash=8B7BCDC2" class="w-full" alt="Tailwind CSS Carousel component" />
+                    </div>
+                    <div class="carousel-item w-full">
+                        <img src="https://api.lorem.space/image/game?w=256&h=400&hash=500B67FB" class="w-full" alt="Tailwind CSS Carousel component" />
+                    </div>
+                    <div class="carousel-item w-full">
+                        <img src="https://api.lorem.space/image/game?w=256&h=400&hash=A89D0DE6" class="w-full" alt="Tailwind CSS Carousel component" />
+                    </div>
+                    <div class="carousel-item w-full">
+                        <img src="https://api.lorem.space/image/game?w=256&h=400&hash=225E6693" class="w-full" alt="Tailwind CSS Carousel component" />
+                    </div>
+                    <div class="carousel-item w-full">
+                        <img src="https://api.lorem.space/image/game?w=256&h=400&hash=9D9539E7" class="w-full" alt="Tailwind CSS Carousel component" />
+                    </div>
+                    <div class="carousel-item w-full">
+                        <img src="https://api.lorem.space/image/game?w=256&h=400&hash=BDC01094" class="w-full" alt="Tailwind CSS Carousel component" />
+                    </div>
+                    <div class="carousel-item w-full">
+                        <img src="https://api.lorem.space/image/game?w=256&h=400&hash=7F5AE56A" class="w-full" alt="Tailwind CSS Carousel component" />
+                    </div>
+                </div>
+            </div> */}
         </body>
     );
 }
